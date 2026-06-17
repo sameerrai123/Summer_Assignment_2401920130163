2e60f56fc7fb934c3dbe2cd2ca67801cee5818b8
Sameer Rai <sameerkumarrai2007@gmail.com>
2026-06-17 16:30:27 +0530

    Day-3 solutions
    Refactor moveZeroes method to improve clarity.

--- Week_1/Day_3/283_Move Zeroes.java
+++ Week_1/Day_3/283_Move Zeroes.java
@@ -1,15 +1,11 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int insertPos = 0;
-
-        // Move non-zero elements forward
+        int insertPos = 0;   
         for (int num : nums) {
             if (num != 0) {
                 nums[insertPos++] = num;
             }
         }
-
-        // Fill remaining positions with 0
         while (insertPos < nums.length) {
             nums[insertPos++] = 0;
         }
